cfe10c2dfa7f04607eb37625fbc5ec316e05fc09
ncl <1820577421@qq.com>
2024-10-12 18:06:05 +0800

    simple auth

--- src/main/java/com/flinform/carpark/controller/AuthController.java
+++ src/main/java/com/flinform/carpark/controller/AuthController.java
@@ -1,30 +1,49 @@
 package com.flinform.carpark.controller;
 
+import com.flinform.carpark.global.JwtAuthenticationResponse;
+import com.flinform.carpark.global.JwtTokenProvider;
+import com.flinform.carpark.global.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-/*    @Autowired
+    @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
     private JwtTokenProvider tokenProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
-        Neo4jProperties.Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = tokenProvider.generateToken(authentication);
-        return ResponseEntity.ok(new JwtAuthenticationResponse(token));
-    }*/
+    public ResponseEntity<JwtAuthenticationResponse> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        try {
+            // 认证用户
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+            // 设置认证信息
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            // 生成令牌
+            String token = tokenProvider.generateToken(authentication);
+            // 返回响应
+            return ResponseEntity.ok(new JwtAuthenticationResponse(token));
+        } catch (AuthenticationException e) {
+            // 认证失败处理
+            //logger.error("Authentication failed", e);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
 }
+
